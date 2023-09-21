ca1a6e651d90f6dbf43b23dc9c6394af1acab0bb
Rhyan Magalhães <rjobdg@outlook.com>
2023-09-21 18:21:17 -0300

    Hello world

---
+++ helloworld/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+            [HttpGet("/meunome")]
+            public string MeuNome()
+            {
+                return "Rhyan!";
+            }
+
+                [HttpGet("/bsms")]
+                public string bsms()
+                {
+                return "1- PROATIVIDADE\n2- RESILIENCIA\n3- ORIENTAÇÃO AO DETALHE";
+                }
+
+                    [HttpGet("/objetivos")]
+                    public string objetivos()
+                    {
+                        return "1- Aprofundar meu conhecimento em Databases\n2- Treinar minha comunicação\n3- Fazer networking";
+                    }
+    }
+}
